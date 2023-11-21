de2377fe63ec8e9517a6a1be3804629c338460a9
Gabriel dos Santos <gabrields1910@gmail.com>
2023-11-20 21:52:22 -0300

    Caso de Uso e Design Patterns - Lidando com erros do use case

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
@@ -17,10 +18,16 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         const registerUseCase = new RegisterUseCase(usersRepository)
 
         await registerUseCase.execute({
-            name, email, password
+            name,
+            email,
+            password,
         })
-    } catch (error) {
-        return reply.status(409).send()
+    } catch (err) {
+        if (err instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({ message: err.message })
+        }
+
+        return reply.status(201).send() // TODO: fix me
     }
 
     return reply.status(201).send()
